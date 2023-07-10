6ef2608a877e0ca97daa4c3f222b78a35ccc06c0
Yann <yannleroy23@gmail.com>
2023-07-10 20:15:45 +0200

    ok

--- components/Home.js
+++ components/Home.js
@@ -36,7 +36,7 @@ function Home() {
 
   // Movies list
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('http://mymoviz-backend-mu-eight.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const formatedData = data.movies.map(movie => {
